4358bd6a702105b72d77a1624c7f5f61406ffa0f
desiderio <desiderio.j@latapult.com>
2025-02-13 18:27:28 -0500

    A starter for working with kysely and building queries

---
+++ src/articles/articles.repository.ts
@@ -0,0 +1,57 @@
+import { Database } from '../database/database'
+import { Article } from './articles.model'
+import { Injectable } from '@nestjs/common'
+import ArticleDto from './dto/article.dto'
+
+@Injectable()
+export class ArticlesRepository {
+    constructor(private readonly database: Database) {}
+
+    async getAll() {
+        const databaseResponse = await this.database
+            .selectFrom('articles')
+            .selectAll()
+            .execute()
+        return databaseResponse.map((articleData) => new Article(articleData))
+    }
+
+    async getById(id: number) {
+        const databaseResponse = await this.database
+            .selectFrom('articles')
+            .where('id', '=', id)
+            .selectAll()
+            .executeTakeFirst()
+
+        if (databaseResponse) {
+            return new Article(databaseResponse)
+        }
+    }
+
+    async create(data: ArticleDto) {
+        const databaseResponse = await this.database 
+         .insertInto('articles')
+         .values({
+             title: data.title,
+             article_content: data.content,
+         })
+         .returningAll()
+         .executeTakeFirst()
+ 
+         if (databaseResponse) return new Article(databaseResponse)
+         else return { id: 1, title: 'test', content: 'test' }
+     }
+ 
+     async update(id: number, data: ArticleDto) {
+         const databaseResponse = await this.database
+             .updateTable('articles')
+             .set({
+                 title: data.title,
+                 article_content: data.content,
+             })
+             .where('id', '=', id)
+             .returningAll()
+             .executeTakeFirst()
+ 
+         if(databaseResponse) return new Article(databaseResponse)
+     }
+}
---
+++ src/database/database.module.ts
@@ -0,0 +1,42 @@
+import { Global, Module } from '@nestjs/common'
+import { 
+    ConfigurableDatabaseModule,
+    DATABASE_OPTIONS    
+} from './database.module-definition'
+import { DatabaseOptions } from './databaseOptions'
+import { Pool } from 'pg'
+import { PostgresDialect } from 'kysely'
+import { Database } from './database'
+
+@Global()
+@Module({
+    exports: [Database],
+    providers: [
+        {
+            provide: Database,
+            inject: [DATABASE_OPTIONS],
+            useFactory: (databaseOptions: DatabaseOptions) => {
+                const dialect = new PostgresDialect({
+                    pool: new Pool({
+                        host: databaseOptions.host,
+                        port: databaseOptions.port,
+                        user: databaseOptions.user,
+                        password: databaseOptions.password,
+                        database: databaseOptions.database,
+                    })
+                })
+
+                return new Database({
+                    dialect,
+                    log(event) {
+                        if (event.level === 'query') {
+                            console.log(event.query.sql)
+                            console.log(event.query.parameters)
+                        }
+                    }
+                })
+            }
+        }
+    ]
+})
+export class DatabaseModule extends ConfigurableDatabaseModule {}
